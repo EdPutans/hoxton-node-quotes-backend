924de0328c89a594758733839a308313e850b3f0
Ed 'Quacker' Putans <edputans@gmail.com>
2021-10-14 14:01:19 +0100

    removed console.log

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,6 @@ app.use(cors({ origin: "*" }));
 const port = 3001;
 
 app.get("/quotes/random", (_, res) => {
-  console.log('called random quotes')
-
   const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomQuoteIndex];
 
